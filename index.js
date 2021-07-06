fb6ff68f9cf678c09df4ee0c6be139a6c1fe53d9
Emanuel Lucas Mizael dos Santos <79867442+mizaelucas@users.noreply.github.com>
2021-07-06 12:31:38 -0300

    finished challenge 32

--- index.js
+++ index.js
@@ -2,36 +2,42 @@
   'use strict';
   
     /*
-    A loja de carros será nosso desafio final. Na aula anterior, você fez a parte
-    do cadastro dos carros. Agora nós vamos começar a deixar ele com cara de
-    projeto mesmo.
-    Crie um novo repositório na sua conta do GitHub, com o nome do seu projeto.
-    Na hora de criar, o GitHub te dá a opção de criar o repositório com um
-    README. Use essa opção.
-    Após criar o repositório, clone ele na sua máquina.
-    Crie uma nova branch chamada `challenge-30`, e copie tudo o que foi feito no
-    desafio da aula anterior para esse novo repositório, nessa branch
-    `challenge-30`.
-    Adicione um arquivo na raiz desse novo repositório chamado `.gitignore`.
-    O conteúdeo desse arquivo deve ser somente as duas linhas abaixo:
-    node_modules
-    npm-debug.log
-    Faça as melhorias que você achar que são necessárias no seu código, removendo
-    duplicações, deixando-o o mais legível possível, e então suba essa alteração
-    para o repositório do seu projeto.
-    Envie um pull request da branch `challenge-30` para a `master` e cole aqui
-    nesse arquivo, dentro do `console.log`, o link para o pull request no seu
-    projeto.
-  */
+    Já temos as funcionalidades de adicionar e remover um carro. Agora, vamos persistir esses dados, 
+    salvando-os temporariamente na memória de um servidor.
+    Nesse diretório do `challenge-32` tem uma pasta `server`. É um servidor simples, em NodeJS, para 
+    que possamos utilizar para salvar as informações dos nossos carros.
+    Para utilizá-lo, você vai precisar fazer o seguinte:
+    - Via terminal, acesse o diretório `server`;
+    - execute o comando `npm install` para instalar as dependências;
+    - execute `node app.js` para iniciar o servidor.
+    Ele irá ser executado na porta 3000, que pode ser acessada via browser no endereço: 
+    `http://localhost:3000`
+    O seu projeto não precisa estar rodando junto com o servidor. Ele pode estar em outra porta.
+    As mudanças que você irá precisar fazer no seu projeto são:
+    - Para listar os carros cadastrados ao carregar o seu projeto, faça um request GET no endereço
+    `http://localhost:3000/car`
+    - Para cadastrar um novo carro, faça um POST no endereço `http://localhost:3000/car`, enviando
+    os seguintes campos:
+      - `image` com a URL da imagem do carro;
+      - `brandModel`, com a marca e modelo do carro;
+      - `year`, com o ano do carro;
+      - `plate`, com a placa do carro;
+      - `color`, com a cor do carro.
+    Após enviar o POST, faça um GET no `server` e atualize a tabela para mostrar o novo carro cadastrado.
+    Crie uma branch `challenge-32` no seu projeto, envie um pull request lá e cole nesse arquivo a URL
+    do pull request.
+    */
 
     console.log(
-    'Link do pull request do seu projeto: https://github.com/mizaelucas/jsninja-challenge30/pull/1');
+      'Link do seu projeto no seu github de forma privada, adicionando anilton.veigaa@gmail.com'
+    );
   
   var app = (function() {
     return {
       init: function() {
         this.companyInfo();
         this.initEvents();
+        this.getCars();
       },
 
 
@@ -43,10 +49,13 @@
       handleSubmit: function handleSubmit(e) {
 
         e.preventDefault();
+        app.postCars();
         console.log('Submitted!');
 
         var $tableCar = DOM('[data-js="table-car"]').get();
         $tableCar.appendChild(app.createNewCar());
+        var tableEl = document.querySelector('table');
+        tableEl.addEventListener('click', app.deleteRow);
       },
 
 
@@ -54,12 +63,15 @@
 
         var $fragment = document.createDocumentFragment();
         var $tr = document.createElement('tr');
+        var $removeButton = document.createElement('button');
         var $tdImage = document.createElement('td');
         var $image = document.createElement('img');
         var $tdBrand = document.createElement('td');
         var $tdYear = document.createElement('td');
         var $tdPlate = document.createElement('td');
         var $tdColor = document.createElement('td');
+
+        $removeButton.className = 'removeBtn';
 
         $image.setAttribute('src', DOM('[data-js="image"]').get().value)
         $tdImage.appendChild($image);
@@ -68,14 +80,25 @@
         $tdYear.textContent = DOM('[data-js="year"]').get().value;
         $tdPlate.textContent = DOM('[data-js="plate"]').get().value;
         $tdColor.textContent = DOM('[data-js="color"]').get().value;
+        $removeButton.textContent = 'Remover';
 
         $tr.appendChild($tdImage);
         $tr.appendChild($tdBrand);
         $tr.appendChild($tdYear);
         $tr.appendChild($tdPlate);
         $tr.appendChild($tdColor);
+        $tr.appendChild($removeButton);
 
         return $fragment.appendChild($tr);
+      },
+
+      deleteRow: function deleteRow(e) {
+        if (!e.target.classList.contains('removeBtn')) {
+          return;
+        }
+
+        var btn = e.target;
+        btn.closest('tr').remove();
       },
 
       companyInfo: function companyInfo() {
@@ -83,7 +106,39 @@
         ajax.open('GET', './company.json', true);
         ajax.send();
         ajax.addEventListener('readystatechange', this.getCompanyInfo, false);
-        console.log('COMPANY INFO', this);
+      },
+
+      getCars: function getCars() {
+        var $tableBody = DOM('[data-js="table-car"]').get();
+        var $fragment = document.createDocumentFragment();
+        var ajax = new XMLHttpRequest();
+        ajax.open('GET', 'http://localhost:3000/car');
+        ajax.send();
+        ajax.onreadystatechange = function () {
+          if (ajax.readyState === 4 && this.status === 200) {
+            Array.prototype.forEach.call(JSON.parse(ajax.responseText), (obj) => {
+              $fragment.appendChild(app.createNewCar(obj))
+            })
+            $tableBody.appendChild($fragment)
+          }
+        }
+      },
+
+      postCars: function postCars() {
+        var ajax = new XMLHttpRequest()
+        var car = {
+          image: DOM('[data-js="image"]').get().value,
+          brandModel: DOM('[data-js="brand-model"]').get().value,
+          year: DOM('[data-js="year"]').get().value,
+          plate: DOM('[data-js="plate"]').get().value,
+          color: DOM('[data-js="color"]').get().value,
+        }
+        ajax.open('POST', 'http://localhost:3000/car')
+        ajax.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+        ajax.send(
+          `image=${car.image}&brandModel=${car.brandModel}&year=${car.year}&plate=${car.plate}&color=${car.color}`
+        )
+        window.location.reload()
       },
 
       getCompanyInfo: function getCompanyInfo() {
